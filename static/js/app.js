d5ffd92c47e0ff3b617d0ca24ebcda3885b76536
P219-C <19741043+P219-C@users.noreply.github.com>
2022-01-03 17:08:24 +0800

    Assignment completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,18 @@
+/**
+ * This function added the word 'OTU' before the OTU id.
+ * Used in the hBar function
+ */
 function labelOTU_id(otu_ids){
     var labeledOTU_ids = otu_ids.map((otu_id) => `OTU ${otu_id}`);
     return labeledOTU_ids
 };
 
+/**
+ * This function plots an horizontal bar chart of the top 10 OTU values of any sample
+ */
 function hBar(hBar_object){
 
-    // Extracting top 10 sample values of sample BB_940
+    // Extracting top 10 sample values
     var top10 = {sample_values: hBar_object.sample_values.slice(0,10), otu_ids: labelOTU_id(hBar_object.otu_ids.slice(0,10)), otu_labels: hBar_object.otu_labels.slice(0,10)}
     // console.log(top10)
 
@@ -26,6 +33,10 @@ function hBar(hBar_object){
     Plotly.newPlot("bar", data, layout);
 };
 
+/**
+ * This function plots a bubble chart of all OTU values of any sample
+ * @param {*} bubble_object Object with information to plot
+ */
 function bubble(bubble_object){
 
     var trace = {
@@ -53,7 +64,10 @@ function bubble(bubble_object){
       Plotly.newPlot('bubble', data, layout);
 };
 
-
+/**
+ * This function creates a panel that displays the metadata associated to any sample
+ * @param {*} objectPanel 
+ */
 function buildPanel(objectPanel){
     var tbody = d3.select("tbody");
     tbody.selectAll("tr").remove()
@@ -63,6 +77,10 @@ function buildPanel(objectPanel){
     });
 };
 
+/**
+ * Simple Gauge Chart displaying Belly Button Washing Frequency of any sample
+ * @param {*} gaugeObject 
+ */
 function gaugeChart(gaugeObject){
     var trace = [
         {
@@ -79,23 +97,22 @@ function gaugeChart(gaugeObject){
 };
 
 
-d3.json("./data/samples.json").then((samplesData)=>{
+d3.json("./data/samples.json").then((samplesData)=>{    // Reading the 'samples.json' file.
     // console.log(samplesData);
 
-    // I.- CREATING INITIAL PLOT (BB_940)
+    // I.- CREATING INITIAL (default) PLOTS for sample BB_940
 
-    // Storing sample BB_940 in a new variable (sampleBB_940)
+    // Storing sample BB_940 in a new variable ('sampleBB_940')
     var sampleBB_940 = samplesData.samples[0];
     // console.log(sampleBB_940)
 
-    // 'sampleBB_940' is already sorted. No need for this step.
+    // 'sampleBB_940' is already sorted. No need to sort (plus it did not work).
     // var sortedBB_940 = sampleBB_940.sample_values.sort((a, b) => {
     //     return b - a;
     // });
 
     // 1. Horizontal Bar Chart   
     hBar(sampleBB_940);
-
 
     // 2. Bubble chart
     bubble(sampleBB_940)
@@ -104,18 +121,22 @@ d3.json("./data/samples.json").then((samplesData)=>{
     var metadataBB_940 = samplesData.metadata[0];
     buildPanel(metadataBB_940);
 
+    // 4. Simple Gauge Chart
     gaugeChart(metadataBB_940)
 
-    // Dropwdown menu
+    // II.- CREATING PLOTS FOR ANY OTHER SAMPLE
+
+    // a. Building the Dropwdown menu
+
     // The following part was adapted from https://stackoverflow.com/questions/20780835/putting-the-country-on-drop-down-list-using-d3-via-csv-file
-    var dropData = samplesData.names.map((otu_id) => {  // Creating an array of objects for the drop down menu
+    var dropData = samplesData.names.map((otu_id) => {  // Creating an Array of Objects with OTU ids for the drop down menu
         // console.log(otu_id)
         return {"value": otu_id};
     });
 
     // console.log(dropData)
-    var selectTag = d3.select("select");    // Reference to the select element
-    var options = selectTag.selectAll("option") // Creating a selection of 'option' elements
+    var selectTag = d3.select("select");    // Reference to the <select> element
+    var options = selectTag.selectAll("option") // Creating a selection of <option> elements
                     .data(dropData)
                     .enter()
                     .append("option");
@@ -123,28 +144,27 @@ d3.json("./data/samples.json").then((samplesData)=>{
     options.text(function (d) {return `BB_${d.value}`;}) // Adding the 'value' attribute and the text content of the option element based on the data
             .attr("value", function(d){return d.value});
 
-    // Event listener
+    // b. Building the Event listener
     d3.select("#selDataset").on("change", () => {
-        var selected = d3.select("#selDataset").property("value");
+        var selected = d3.select("#selDataset").property("value");  // Extracting the value of the selected <option>
         // console.log(selected)
 
-        selectedSample = samplesData.samples.forEach((sample) => {
+        selectedSample = samplesData.samples.forEach((sample) => {  // Looping through all the samples
             // console.log(sample.id)
-            if (sample.id == selected) {
+            if (sample.id == selected) {                            // Finding and plotting the selected sample
                 hBar(sample);
                 bubble(sample);
             }
         });
 
-        selectedSample = samplesData.metadata.forEach((sample) => {
+        selectedSample = samplesData.metadata.forEach((sample) => { // Looping through all the samples
             // console.log(sample.id)
-            if (sample.id == selected) {
+            if (sample.id == selected) {                            // Finding and plotting the selected sample
                 buildPanel(sample);
                 gaugeChart(sample);
             }
         });
 
     });
-
-    
+  
 });
